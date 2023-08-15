33ce1ea322ef26a6360ce89e84debb791854cd63
wikijm <wikijm@users.noreply.github.com>
2023-08-15 18:57:48 +0200

    Update CreateMutexes.cpp to v1.1
    Tuning script info

--- CreateMutexes.cpp
+++ CreateMutexes.cpp
@@ -13,59 +13,92 @@ std::wstring ReplaceString(std::wstring str, const std::wstring& target, const s
     return str;
 }
 
-int main() {
-    // Get the computer name
-    wchar_t computerName[MAX_COMPUTERNAME_LENGTH + 1];
-    DWORD computerNameSize = sizeof(computerName) / sizeof(computerName[0]);
-    if (!GetComputerName(computerName, &computerNameSize)) {
-        std::wcerr << L"Failed to get computer name, Error: " << GetLastError() << std::endl;
-        return 1;
-    }
-    
-    // Display script information
+void LogError(const std::wstring& message) {
+    std::wcerr << L"Error: " << message << L", Error Code: " << GetLastError() << std::endl;
+}
+
+void DisplayScriptInfo(const std::wstring& computerName) {
     std::wcout << L"Script Name:             CreateMutexes\n";
-    std::wcout << L"Script Version:          1.0\n";
+    std::wcout << L"Script Version:          1.1\n";
+}
+
+bool GetComputerNameSafe(wchar_t* computerName, DWORD* size) {
+    if (!GetComputerName(computerName, size)) {
+        LogError(L"Failed to get computer name");
+        return false;
+    }
+    return true;
+}
+
+void DisplayExecutionInfo(const std::wstring& computerName) {
     std::wcout << L"Executing on Machine:    " << computerName << std::endl;
     std::wcout << L"\n\n";
+}
 
-    // Read mutex names from file
-    std::wifstream mutexFile(L"mutex.list");
+bool ReadMutexNamesFromFile(const std::wstring& filePath, std::vector<std::wstring>& mutexNames, const std::wstring& computerName) {
+    std::wifstream mutexFile(filePath);
     if (!mutexFile.is_open()) {
-        std::wcerr << L"Failed to open mutex list file." << std::endl;
-        return 1;
+        LogError(L"Failed to open mutex list file.");
+        return false;
     }
 
-    std::vector<std::wstring> mutexNames;
     std::wstring line;
     while (std::getline(mutexFile, line)) {
         line = ReplaceString(line, L"[ComputerName]", computerName);
         mutexNames.push_back(line);
     }
     mutexFile.close();
+    return true;
+}
 
-    // Create mutexes
-    std::vector<HANDLE> mutexHandles;
-
+bool CreateMutexes(const std::vector<std::wstring>& mutexNames, std::vector<HANDLE>& mutexHandles) {
     for (const auto& mutexName : mutexNames) {
         HANDLE mutexHandle = CreateMutex(NULL, FALSE, mutexName.c_str());
         if (mutexHandle == NULL) {
-            std::wcerr << L"Failed to create mutex: " << mutexName << L", Error: " << GetLastError() << std::endl;
+            LogError(L"Failed to create mutex: " + mutexName);
         }
         else {
             mutexHandles.push_back(mutexHandle);
             std::wcout << L"Mutex added: " << mutexName << std::endl;
         }
     }
+    return true;
+}
 
-    // Wait for user input to release mutexes
-    std::wcout << L"Press Enter to release mutexes..." << std::endl;
-    std::wcin.get();
-
-    // Release mutexes
+void ReleaseMutexes(const std::vector<HANDLE>& mutexHandles) {
     for (auto mutexHandle : mutexHandles) {
         ReleaseMutex(mutexHandle);
         CloseHandle(mutexHandle);
     }
+}
+
+int main() {
+    // Get the computer name
+    wchar_t computerName[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD computerNameSize = sizeof(computerName) / sizeof(computerName[0]);
+    if (!GetComputerNameSafe(computerName, &computerNameSize)) {
+        return 1;
+    }
+
+    DisplayScriptInfo(computerName);
+
+    DisplayExecutionInfo(computerName);
+
+    std::vector<std::wstring> mutexNames;
+    if (!ReadMutexNamesFromFile(L"mutex.list", mutexNames, computerName)) {
+        return 1;
+    }
+
+    std::vector<HANDLE> mutexHandles;
+    if (!CreateMutexes(mutexNames, mutexHandles)) {
+        ReleaseMutexes(mutexHandles);
+        return 1;
+    }
+
+    std::wcout << L"Press Enter to release mutexes..." << std::endl;
+    std::wcin.get();
+
+    ReleaseMutexes(mutexHandles);
 
     return 0;
 }
